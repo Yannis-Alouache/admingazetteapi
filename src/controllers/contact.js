3d0568e6a8aed881b74f8083503e3db913a8b850
Rayan Desfossez <layl59@protonmail.ch>
2021-07-15 17:17:09 +0200

    add get contact

--- src/controllers/contact.js
+++ src/controllers/contact.js
@@ -241,7 +241,66 @@ exports.Contact = class Contact {
     }
 
     async get(req, res) {
+        const {
+            token,
+            email,
+        } = req.body
 
+        if (!token) {
+            return {
+                status: "error",
+                message: "Merci de vous connectez !"
+            }
+        }
+
+        try {
+            const decoded = await jwt.verify(token, (await this.env.get("SECRET")))
+
+            if (decoded.email === email || !email) {
+                const user = await this.users.findOne({ email: decoded.email })
+
+                for (const p of user.permissions) {
+                    if (p === "GET_CONTACT") {
+                        return {
+                            status: "success",
+                            data: {
+                                contacts: user.contacts
+                            }
+                        }
+                    }
+                }
+            } else {
+                const [
+                    me,
+                    user
+                ] = await Promise.all([
+                    this.users.findOne({ email: decoded.email }),
+                    this.users.findOne({ email })
+                ])
+
+                for (const p of me.permissions) {
+                    if (p === "GET_CONTACT") {
+                        return {
+                            status: "success",
+                            data: {
+                                contacts: user.contacts
+                            }
+                        }
+                    }
+                }
+            }
+
+            return {
+                status: "error",
+                message: "Vous n'avez pas la permission"
+            }
+        }
+         catch (e) {
+            return {
+                status: "error",
+                message: e
+            }
+        }
     }
 }
 

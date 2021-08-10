010d1c051ec16b726f8cb51454575bfe942358f5
Yannis Alouache <alouacehyannis1@gmail.com>
2021-08-10 11:30:59 +0200

    [~] fixed contact filter in Delete function

--- src/controllers/contact.js
+++ src/controllers/contact.js
@@ -169,7 +169,7 @@ exports.Contact = class Contact {
 
                 for (const p of user.permissions) {
                     if (p === PermissionType.DELETE_CONTACT) {
-                        user.contacts = user.contacts.filter(e => e.tel !== contact.tel)
+                        user.contacts = user.contacts.filter(e => e.tel !== contact[0].tel)
                         Promise.all([
                             this.users.updateOne({
                                 "email": decoded.email
@@ -199,7 +199,7 @@ exports.Contact = class Contact {
 
                 for (const p of me.permissions) {
                     if (p === PermissionType.DELETE_CONTACT) {
-                        user.contacts = user.contacts.filter(e => e.tel !== contact.tel)
+                        user.contacts = user.contacts.filter(e => e.tel !== contact[0].tel)
                         Promise.all([
                             this.users.updateOne({
                                 "email": email
